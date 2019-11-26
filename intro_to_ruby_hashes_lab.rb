7479fa050c3c16f0727a8ab494ff5f21dd9e5906
Jesse Tippit <jtippit223@gmail.com>
2019-11-26 06:58:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -64,7 +64,6 @@ def monopoly_with_fourth_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
  monopoly = {:railroads => {
-end
     :pieces => 4,
 
     :names => {:reading_railroad => {"mortgage_value" => "$100"}, :pennsylvania_railroad => {"mortgage_value" => "$200"}, :b_and_o_railroad => {"mortgage_value" => "$400"}, :shortline => {"mortgage_value" => "$800"} },
